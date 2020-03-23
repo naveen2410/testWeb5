c9d83cbf3fda4faf59e9aad04cd2b5449e68f00f
NaveenGoyal <NaveenGoyal@DESKTOP-O94QS2S>
2020-03-23 14:35:54 +0530

    sads

--- src/main/java/testjava5/testjava5/MyResource.java
+++ src/main/java/testjava5/testjava5/MyResource.java
@@ -23,7 +23,7 @@ public class MyResource {
     public String getIt() {
     	TestMIIClient ncg = new TestMIIClient();			
 		
-        return "Got it!" + ncg.executeGETService();
+        return "Got it! 1" + ncg.executeGETService();
     }
     
     
